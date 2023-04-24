f023b7f5e118c8801068ed0754feab294563c2c3
Themba <seconds.seconds1@gmail.com>
2023-04-24 09:40:14 -0700

    ffl

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
 
-void bubble_sort(int *array, size_t size){
+/**
+ * bubble_sort - sort arrays of inegers
+ *
+ * @array:array to be sorted
+ * @size: size of the arr
+ * Returns: nothing.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
 	int swp  = 1;
-	while (swp) {
+
+	while (swp)
+	{
 		swp = 0;
-		for (size_t i = 0; i < size - 1; i++){
-			if (array[i] > array[i + 1]){
+		for (size_t i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
 				int temp = array[i];
+
 				array[i] = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				swp = 1;
-				printf(array[i], array[i+1]);
+				printf(array[i], array[i + 1]);
 			}
 		}
 		size--;
