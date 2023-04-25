67de17b8dbf3e633a760279481815c0fd36b6df0
Themba <seconds.seconds1@gmail.com>
2023-04-25 08:39:10 -0700

    loo

---
+++ print_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of int
+ *
+ * @array: thr array
+ * @size: number
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+		{
+			printf(", ");
+		}
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
+}
